8d0c2768efe00f191b96c1d1b521c872e6b6c5ea
frabxony <bruno.okafor@gmail.com>
2018-01-18 09:46:41 +0100

    First Commit

---
+++ src/main/java/com/gapsobackend/GapsoBackendApplication.java
@@ -0,0 +1,12 @@
+package com.gapsobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GapsoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GapsoBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gapsobackend/GapsoBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gapsobackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GapsoBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
